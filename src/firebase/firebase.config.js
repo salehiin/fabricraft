42de3e15caf7532f50cb83f17396d6d5ef6a6bb9
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-04-30 18:21:38 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -8,14 +8,18 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+console.log(import.meta.env.VITE_PASS)
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6h_XUNIP_Lu07te3wLiF7xiIRVnaDfak",
-  authDomain: "aten-fabricraft.firebaseapp.com",
-  projectId: "aten-fabricraft",
-  storageBucket: "aten-fabricraft.appspot.com",
-  messagingSenderId: "1033780694565",
-  appId: "1:1033780694565:web:33ec35658af0d63e3379cc"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
